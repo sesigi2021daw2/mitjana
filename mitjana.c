e0808cf5e32893257ca17b59c20925bb8b3d8aaf
Sergi <Sergitgna@gmail.com>
2020-12-16 22:29:10 +0100

    Tercer commit del programa mitjana

--- mitjana.c
+++ mitjana.c
@@ -1,8 +1,8 @@
            #include <stdio.h>
            
            int main() {
-           	float suma=0;
-           	float mitjana;
+           	float suma=0.0;
+           	float mitjana=0.0;
            	float nou_valor;
            	int numero_repeticions=0;
            	char consulta='s';
@@ -13,7 +13,7 @@
            		suma=suma+nou_valor;
            		numero_repeticions=numero_repeticions+1;
            		printf("Vols continuar (s/n): ");
-           		scanf(" %c",&consulta); 
+           		scanf("%s",&consulta); 
            	}
            	mitjana=suma/numero_repeticions;
            	printf("La mitjana resultant és: %.2f\n",mitjana);	
